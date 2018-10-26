2672ce716d73f6f7a78d4f9f8754c4e37d2b3be4
Yoeri Van Damme <Yoeri.VanDamme@sdworx.com>
2018-10-26 12:40:34 +0200

    more comments

--- Container.cs
+++ Container.cs
@@ -22,9 +22,13 @@ namespace InjectionByExample
         public object Resolve(Type t)
         {
             if (!_registrations.ContainsKey(t)) throw new Exception($"Type {t.Name} is not registered.");
+            // get the registration first
             var registration = _registrations[t];
+            // determine the activator based on the lifetime of the registration
             var activator = _activators.First(act => act.ForLifeTime == registration.Lifetime);
+            // create an instance
             var instance = activator.CreateInstance(this, registration);
+            // return the instance
             return instance;
         }
 
@@ -34,10 +38,19 @@ namespace InjectionByExample
             return (T)this.Resolve(t);
         }
 
+        /*
+        Very popular feature is to create child containers. We can do this by handing down the existing registrations
+        and of course a reference to the parent (this).
+         */
         public Container CreateChild()
         {
             return new Container(this._registrations, this);
         }
+
+        /*
+        This method is only for the InstancePerContainerActivator.
+        This is used to ask the parent container if an instance already exists.
+         */
         public bool HasInstance(Type t)
         {
             return this._activators
